823a31bff098de656b3e7a27341b562ae217ab9c
pythonGeek <stephen.kanyi@andela.com>
2016-12-07 12:39:06 +0300

    Fix missing arguments bug

--- test/test_person.py
+++ test/test_person.py
@@ -27,14 +27,14 @@ class TestPerson(TestCase):
         self.assertEqual(staff1.position.lower(), 'staff', 'The position must be a staff')
 
     def test_allocate_office(self):
-        Amity.create_room('Carmel')
+        Amity.create_room('Carmel', 'office')
         self.assertTrue('Carmel' in Amity.office_rooms)
         Amity.allocate_office('Carmel')
         self.assertNotEqual(len(Amity.office_rooms['Carmel']), 0, 'person1 has not been added to the office')
 
     def test_allocate_living_space(self):
-        Amity.create_room('PHP')
-        self.assertTrue('PHP', in Amity.ls_rooms)
+        Amity.create_room('PHP', 'livingspace')
+        self.assertTrue('PHP' in Amity.ls_rooms)
         Amity.allocate_living_space('PHP')
         self.assertNotEqual(len(Amity.ls_rooms['PHP']), 0, 'person1 has not been added to living space')
 
